a6cc0bce792618e54ba5b79b55b8d30705d36dab
danyentezari <dany.entezari@gmail.com>
2021-05-12 20:56:39 +0400

    Added ContactScreen.js.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MainScreen from './MainScreen';
 import AboutScreen from './AboutScreen';
+import ContactScreen from './ContactScreen';
 import LayoutRoute from "./LayoutRoute";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
             <Switch>
                 <LayoutRoute path="/" exact={true} component={MainScreen} />
                 <LayoutRoute path="/about" exact={true} component={AboutScreen} />
+                <LayoutRoute path="/contact" exact={true} component={ContactScreen} />
             </Switch>
         </BrowserRouter>
     )
