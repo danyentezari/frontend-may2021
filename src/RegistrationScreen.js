3d212f854b147653b9af1cf575426724be3cf273
danyentezari <dany.entezari@gmail.com>
2021-05-15 13:26:05 +0400

    Setup for fetch.

--- src/RegistrationScreen.js
+++ src/RegistrationScreen.js
@@ -5,6 +5,48 @@ function RegistrationScreen() {
     // "initial", "sending", "successful", "unsuccessful", "validation error"
     const [state, setState] = useState("initial"); 
 
+    let firstNameField;
+    let lastNameField;
+    let emailField;
+    let passwordField;
+    let contactField;
+    let addressField;
+
+    const register = () => {
+
+        const errors = [];
+
+        // 1. Validate the fields
+        if(firstNameField.value.length == 0) {
+            errors.push("Please enter your first name");
+        }
+        if(lastNameField.value.length == 0) {
+            errors.push("Please enter your last name");
+        }
+        if(emailField.value.length == 0) {
+            errors.push("Please enter a valid email address");
+        }
+        if(passwordField.value.length == 0) {
+            errors.push("Please enter a valid password");
+        }
+
+        // 1.1 If there are errors, set the state to "validation error"
+        if(errors.length > 0) {
+            setState("validation error");
+        }
+        // 1.2 If there are no errors, set the state to "sending"
+        else {
+            setState("sending");
+
+            fetch('')
+            // 2.1 If the submission is successful, set the state "successful"
+            .then((backendResponse)=> backendResponse.json())
+            .then((theJson)=>console.log(theJson))
+            // 2.2 If the submission is unsuccessful, set the state "unsuccessful"
+            .catch((error)=>console.log(error))
+        }
+    }
+
     return (
         <div className="container" style={{maxWidth: 600, minHeight: 'calc(100vh - 112px)'}}>
 
@@ -12,38 +54,40 @@ function RegistrationScreen() {
 
             <div className="mb-3">
                 <label for="firstName" class="form-label">First Name</label>
-                <input type="text" class="form-control" id="firstName" aria-describedby="firstName" />
+                <input ref={ (elem)=>firstNameField = elem } type="text" class="form-control" id="firstName" aria-describedby="firstName" />
             </div>
 
             <div className="mb-3">
                 <label for="lastName" class="form-label">Last Name</label>
-                <input type="text" class="form-control" id="lastName" aria-describedby="lastName" />
+                <input ref={ (elem)=>lastNameField = elem } type="text" class="form-control" id="lastName" aria-describedby="lastName" />
             </div>
 
             <div className="mb-3">
                 <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" />
+                <input ref={ (elem)=>emailField = elem } type="email" class="form-control" id="email" aria-describedby="emailHelp" />
                 <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
 
             <div className="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" aria-describedby="password" />
+                <input ref={ (elem)=>passwordField = elem } type="password" class="form-control" id="password" aria-describedby="password" />
             </div>
 
             <div className="mb-3">
                 <label for="contactNumber" class="form-label">Contact Number</label>
-                <input type="text" class="form-control" id="contactNumber" aria-describedby="contactNumber" />
+                <input ref={ (elem)=>contactField = elem } type="text" class="form-control" id="contactNumber" aria-describedby="contactNumber" />
             </div>
 
             <div className="mb-3">
                 <label for="address" class="form-label">Address</label>
-                <input type="text" class="form-control" id="address" aria-describedby="address" />
+                <input ref={ (elem)=>addressField = elem } type="text" class="form-control" id="address" aria-describedby="address" />
             </div>
 
             {
                 state !== "sending" && state !== "successful" &&
-                <button className="btn btn-primary mb-3" type="button">Submit</button>
+                <button 
+                onClick={register}
+                className="btn btn-primary mb-3" type="button">Submit</button>
             }
 
             { 
@@ -71,7 +115,6 @@ function RegistrationScreen() {
                 state === "sending" &&
                 <p>Loading...</p>
             }
-
 
 
         </div>
